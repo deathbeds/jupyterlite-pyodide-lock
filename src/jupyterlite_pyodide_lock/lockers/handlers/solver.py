785fb8dcfdb0d21829fa8b5180512df8ff0a1305
Nicholas Bollweg <nick.bollweg@gmail.com>
2024-04-02 16:55:36 -0500

    move template to file

--- src/jupyterlite_pyodide_lock/lockers/handlers/solver.py
+++ src/jupyterlite_pyodide_lock/lockers/handlers/solver.py
@@ -1,77 +1,29 @@
+from pathlib import Path
 from typing import TYPE_CHECKING
 
 import jinja2
+from jupyterlite_core.constants import UTF8
 from tornado.web import RequestHandler
 
 if TYPE_CHECKING:  # pragma: no cover
     from logging import Logger
 
-TEMPLATE = """
-    <html>
-        <script type="module">
-            import { loadPyodide } from './static/pyodide/pyodide.mjs';
-
-            async function post(url, body) {
-                return await fetch(
-                    url, {
-                    method: "POST",
-                    headers: {"Content-Type": "application/json"},
-                    body
-                });
-            }
-
-            function tee(pipe, message) {
-                console.log(pipe, message);
-                void post(`/log/${pipe}`, JSON.stringify({ message }, null, 2));
-                const pre = document.window.createElement('pre');
-                pre.textContent = message;
-                document.body.appendChild(pre);
-            }
-
-            const pyodide = await loadPyodide({
-                stdout: tee.bind(this, 'stdout'),
-                stderr: tee.bind(this, 'stderr'),
-                packages: ["micropip"],
-            });
-
-            await pyodide.runPythonAsync(`
-                try:
-                    import micropip, js, json
-                    await micropip.install(
-                        **json.loads(
-                            '''
-                            {{ micropip_args_json }}
-                            '''
-                        )
-                    )
-                    js.window.PYODIDE_LOCK = micropip.freeze()
-                except Exception as err:
-                    js.window.PYODIDE_ERROR = str(err)
-            `);
-
-            await post(
-                "./pyodide-lock.json",
-                window.PYODIDE_LOCK
-                    || JSON.stringify({"error": window.PYODIDE_ERROR})
-            );
-            window.close();
-        </script>
-    </html>
-"""
-
 
 class SolverHTML(RequestHandler):
-    context: dict
+    context: dict[str, str]
     log: "Logger"
+    template: jinja2.Template
 
     def initialize(self, context, *args, **kwargs):
         log = kwargs.pop("log")
         super().initialize(*args, **kwargs)
         self.context = context
         self.log = log
+        self.template = jinja2.Template(
+            (Path(__file__).parent / "lock.html.j2").read_text(**UTF8)
+        )
 
     async def get(self, *args, **kwargs):
-        template = jinja2.Template(TEMPLATE)
-        rendered = template.render(self.context)
+        rendered = self.template.render(self.context)
         self.log.debug("Serving HTML\n%s", rendered)
         await self.finish(rendered)
