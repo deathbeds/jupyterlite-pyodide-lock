017b76aca25356ee253bd41ec2016d0410cad8d8
Nicholas Bollweg <nick.bollweg@gmail.com>
2024-12-22 12:31:00 -0600

    fix webdriver test

--- tests/test_repo.py
+++ tests/test_repo.py
@@ -10,14 +10,14 @@ from .conftest import ROOT, UTF8
 
 PIXI_PATTERNS = {
     ".github/workflows/*.yml": r"JLPL_PIXI_VERSION: ([^\s]+)",
-    "docs/.readthedocs.yaml": r"pixi ==([^\s]+)",
+    "docs/.readthedocs.yaml": r"""pixi ==([^\s"']+)""",
     "CONTRIBUTING.md": r"pixi==([^\s]+)",
 }
 
 
 @pytest.mark.parametrize(("glob"), PIXI_PATTERNS.keys())
 def test_repo_pixi_version(the_pixi_version: str, glob: str) -> None:
-    """Verify consistent pixi versions."""
+    """Verify consistent ``pixi`` versions."""
     paths = sorted(ROOT.glob(glob))
     assert paths
     for path in paths:
